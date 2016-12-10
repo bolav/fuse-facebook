686258e3cc49ac24c262e77376e06ac4f8ac0650
Bjorn-Olav Strand <bolav@ikke.no>
2016-12-10 10:48:49 +0100

    Moved to new structure

--- libs/get_ios.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir dl
-curl 'https://origincache.facebook.com/developers/resources/?id=facebook-ios-sdk-current.zip' > dl/facebook-ios-sdk-current.zip
-mkdir ios
-cd ios
-unzip ../dl/facebook-ios-sdk-current.zip
